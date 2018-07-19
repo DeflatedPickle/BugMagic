8e7c7ec89367fcc22c064ff675b07c2bcf26a6dd
DeflatedPickle <DeflatedPickle@gmail.com>
2018-07-19 19:29:00 +0100

    Added main objects

---
+++ src/main/kotlin/com/deflatedpickle/bugmagic/BugMagic.kt
@@ -0,0 +1,24 @@
+package com.deflatedpickle.bugmagic
+
+import com.deflatedpickle.bugmagic.proxy.CommonProxy
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.SidedProxy
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS, dependencies = Reference.DEPENDENCIES, modLanguageAdapter = Reference.ADAPTER)
+object BugMagic {
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    var proxy: CommonProxy? = null
+
+    val log: Logger = LogManager.getLogger(Reference.MOD_ID)
+
+    @EventHandler
+    fun preInit(event: FMLPreInitializationEvent) {
+        log.info("Starting preInit.")
+        proxy!!.preInit(event)
+        log.info("Finished preInit.")
+    }
+}
