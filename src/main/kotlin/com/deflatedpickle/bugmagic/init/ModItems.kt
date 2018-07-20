2d3435fa1be7d6664a6eda2cd5d2fe19cd91a8cc
DeflatedPickle <DeflatedPickle@gmail.com>
2018-07-20 04:56:51 +0100

    Added a bug net

--- src/main/kotlin/com/deflatedpickle/bugmagic/init/ModItems.kt
+++ src/main/kotlin/com/deflatedpickle/bugmagic/init/ModItems.kt
@@ -1,7 +1,10 @@
 package com.deflatedpickle.bugmagic.init
 
+import com.deflatedpickle.bugmagic.items.ItemBugNet
 import com.deflatedpickle.bugmagic.items.ItemWand
 
 object ModItems {
     val wandGeneric = ItemWand("wand_generic", 1, ModCreativeTabs.tabGeneral)
+
+    val bugNet = ItemBugNet("bug_net", 1, ModCreativeTabs.tabGeneral, 5)
 }
---
+++ src/main/kotlin/com/deflatedpickle/bugmagic/items/ItemBugNet.kt
@@ -0,0 +1,52 @@
+package com.deflatedpickle.bugmagic.items
+
+import com.deflatedpickle.picklelib.item.ItemBase
+import net.minecraft.creativetab.CreativeTabs
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NBTTagCompound
+import net.minecraft.util.ActionResult
+import net.minecraft.util.EnumHand
+import net.minecraft.world.World
+
+class ItemBugNet(name: String, stackSize: Int, creativeTab: CreativeTabs, val bugLimit: Int) : ItemBase(name, stackSize, creativeTab) {
+    override fun onItemRightClick(worldIn: World, playerIn: EntityPlayer, handIn: EnumHand): ActionResult<ItemStack> {
+        if (!playerIn.world.isRemote) {
+            val stack = playerIn.getHeldItem(handIn)
+
+            if (!stack.hasTagCompound()) {
+                stack.tagCompound = NBTTagCompound()
+            }
+
+            // TODO: Change this to catch a small portion of the amount of bugs in the biome, based on biome type and size
+            val caughtAmount = (0..3).shuffled().last()
+
+            val compound = stack.tagCompound!!
+            val bugAmount = compound.getInteger("bugs")
+
+            if (bugAmount < bugLimit) {
+                if (bugAmount + caughtAmount < bugLimit) {
+                    compound.setInteger("bugs", caughtAmount + bugAmount)
+                }
+                else {
+                    compound.setInteger("bugs", bugLimit)
+                }
+            }
+        }
+
+        return super.onItemRightClick(worldIn, playerIn, handIn)
+    }
+
+    override fun showDurabilityBar(stack: ItemStack?): Boolean {
+        return true
+    }
+
+    override fun getDurabilityForDisplay(stack: ItemStack?): Double {
+        return if (stack!!.hasTagCompound()) {
+            (bugLimit - stack.tagCompound!!.getInteger("bugs").toDouble()) / bugLimit
+        }
+        else {
+            1.0
+        }
+    }
+}
