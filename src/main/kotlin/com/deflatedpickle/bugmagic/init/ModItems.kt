88b4e2c057b0a4ac2d057e904ab4c68aeeab95a7
DeflatedPickle <DeflatedPickle@gmail.com>
2018-07-25 09:44:37 +0100

    Added a spell parchment item

--- src/main/kotlin/com/deflatedpickle/bugmagic/init/ModItems.kt
+++ src/main/kotlin/com/deflatedpickle/bugmagic/init/ModItems.kt
@@ -1,10 +1,15 @@
 package com.deflatedpickle.bugmagic.init
 
 import com.deflatedpickle.bugmagic.items.ItemBugNet
+import com.deflatedpickle.bugmagic.items.ItemSpellParchment
 import com.deflatedpickle.bugmagic.items.ItemWand
+import com.deflatedpickle.bugmagic.spells.SpellFirefly
 
 object ModItems {
     val wandGeneric = ItemWand("wand_generic", 1, ModCreativeTabs.tabGeneral)
 
     val bugNet = ItemBugNet("bug_net", 1, ModCreativeTabs.tabGeneral, 5)
+
+    // Spells
+    val spellFirefly = ItemSpellParchment("spell_firefly", 1, ModCreativeTabs.tabGeneral, SpellFirefly())
 }
