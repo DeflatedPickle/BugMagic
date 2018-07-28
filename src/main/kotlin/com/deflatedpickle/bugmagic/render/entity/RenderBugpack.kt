7749d088b8c2dd238e97b969483881ff070bf520
DeflatedPickle <DeflatedPickle@gmail.com>
2018-07-28 12:32:45 +0100

    Added a chest to the render

--- src/main/kotlin/com/deflatedpickle/bugmagic/render/entity/RenderBugpack.kt
+++ src/main/kotlin/com/deflatedpickle/bugmagic/render/entity/RenderBugpack.kt
@@ -18,4 +18,28 @@ class RenderBugpack(renderManager: RenderManager) : RenderLiving<EntityBugpack>(
     override fun getEntityTexture(entity: EntityBugpack?): ResourceLocation? {
         return texture
     }
+
+    override fun doRender(entity: EntityBugpack, x: Double, y: Double, z: Double, entityYaw: Float, partialTicks: Float) {
+        val ny = y + 0.33f
+        super.doRender(entity, x, ny, z, entityYaw, partialTicks)
+
+        GlStateManager.pushMatrix()
+        run {
+            // Move the chest to the bugs location
+            GlStateManager.translate(x, ny, z)
+            GlStateManager.rotate(-entityYaw, 0f, 1f, 0f)
+
+            // Rotate the chest to stand "up"
+            GlStateManager.rotate(180f, 1f, 0f, 0f)
+            GlStateManager.scale(0.35f, 0.35f, 0.35f)
+
+            GlStateManager.translate(-0.5f, -0.3f, -0.45f)
+            GlStateManager.rotate(15f, 1f, 0f, 0f)
+            GlStateManager.translate(0.005f, 0.2f, -0.1f)
+
+            this.bindTexture(chest_texture)
+            chest_model.renderAll()
+        }
+        GlStateManager.popMatrix()
+    }
 }
