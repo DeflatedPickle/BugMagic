e85b7a8356b960f4ae87cafe9d654d0a91c16d1a
DeflatedPickle <DeflatedPickle@gmail.com>
2018-08-07 16:36:53 +0100

    Updated EntityAIHoverToOwner

--- src/main/kotlin/com/deflatedpickle/bugmagic/entity/ai/EntityAIHoverToOwner.kt
+++ src/main/kotlin/com/deflatedpickle/bugmagic/entity/ai/EntityAIHoverToOwner.kt
@@ -22,14 +22,20 @@ class EntityAIHoverToOwner(private val entityIn: EntityTameable, private val flo
     override fun updateTask() {
         // TODO: Make the mob face the same way as the owner
         // TODO: Fix the rotation movement, so that the mob sticks to the front-left of the owner
+        // if (this.entityIn.posY < this.entityIn.owner!!.posY + 1.5f) {
+        //     this.entityIn.moveRelative(this.entityIn.owner!!.posX.toFloat() - this.entityIn.posX.toFloat() - offsetX + Math.sin(this.entityIn.owner!!.rotationYaw.toDouble()).toFloat(),
+        //             this.floatHeightIn,
+        //             this.entityIn.owner!!.posZ.toFloat() - this.entityIn.posZ.toFloat() - offsetZ + Math.cos(this.entityIn.owner!!.rotationYaw.toDouble()).toFloat(),
+        //             0.75f)
+        // }
 
         // this.entityIn.rotationYaw = 0f
-        // this.entityIn.setRotationYawHead(this.entityIn.owner!!.rotationYawHead)
+        this.entityIn.setRotationYawHead(this.entityIn.owner!!.rotationYawHead)
 
-        if (this.entityIn.posY < this.entityIn.owner!!.posY + 1.5f) {
-            this.entityIn.moveRelative(this.entityIn.owner!!.posX.toFloat() - this.entityIn.posX.toFloat() - offsetX + Math.sin(this.entityIn.owner!!.rotationYaw.toDouble()).toFloat(),
+        if (this.entityIn.posY < this.entityIn.owner!!.posY + 1.7f) {
+            this.entityIn.moveRelative(this.entityIn.owner!!.posX.toFloat() - this.entityIn.posX.toFloat() - offsetX,
                     this.floatHeightIn,
-                    this.entityIn.owner!!.posZ.toFloat() - this.entityIn.posZ.toFloat() - offsetZ + Math.cos(this.entityIn.owner!!.rotationYaw.toDouble()).toFloat(),
+                    this.entityIn.owner!!.posZ.toFloat() - this.entityIn.posZ.toFloat() - offsetZ,
                     0.75f)
         }
     }
