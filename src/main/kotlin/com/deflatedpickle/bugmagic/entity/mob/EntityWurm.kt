1db0d98db0522438197fbcbfd28e7f0e9f429897
DeflatedPickle <DeflatedPickle@gmail.com>
2018-08-15 09:36:30 +0100

    Added the AI task

--- src/main/kotlin/com/deflatedpickle/bugmagic/entity/mob/EntityWurm.kt
+++ src/main/kotlin/com/deflatedpickle/bugmagic/entity/mob/EntityWurm.kt
@@ -1,5 +1,6 @@
 package com.deflatedpickle.bugmagic.entity.mob
 
+import com.deflatedpickle.bugmagic.entity.ai.EntityAIDigDown
 import net.minecraft.entity.EntityAgeable
 import net.minecraft.entity.SharedMonsterAttributes
 import net.minecraft.entity.passive.EntityTameable
@@ -27,6 +28,7 @@ class EntityWurm(worldIn: World) : EntityTameable(worldIn) {
 
     override fun initEntityAI() {
         // TODO: Add an AI to dig down to bedrock, if the block below is bedrock, die
+        this.tasks.addTask(2, EntityAIDigDown(this))
     }
 
     override fun applyEntityAttributes() {
