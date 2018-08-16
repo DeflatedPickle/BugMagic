d75ccc50d9ca5dfd059438a96de61d39fe4d1bcb
DeflatedPickle <DeflatedPickle@gmail.com>
2018-08-16 07:53:39 +0100

    Updated in-game version

--- src/main/kotlin/com/deflatedpickle/bugmagic/Reference.kt
+++ src/main/kotlin/com/deflatedpickle/bugmagic/Reference.kt
@@ -4,7 +4,7 @@ object Reference {
     const val MOD_ID = "bugmagic"
     const val NAME = "BugMagic"
     // Versions follow this format: MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
-    const val VERSION = "1.12.2-0.0.12.10"
+    const val VERSION = "1.12.2-0.0.15.1"
     const val ACCEPTED_VERSIONS = "[1.12.1, 1.12.2]"
 
     const val CLIENT_PROXY_CLASS = "com.deflatedpickle.bugmagic.proxy.ClientProxy"
