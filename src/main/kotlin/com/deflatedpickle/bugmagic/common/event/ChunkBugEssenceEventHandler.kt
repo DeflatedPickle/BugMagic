699610fbfc3dc98afb5c1f70de6a3c78edeade35
DeflatedPickle <DeflatedPickle@gmail.com>
2020-12-01 23:39:41 +0000

    Stopped chunks spawning with existing essence

--- src/main/kotlin/com/deflatedpickle/bugmagic/common/event/ChunkBugEssenceEventHandler.kt
+++ src/main/kotlin/com/deflatedpickle/bugmagic/common/event/ChunkBugEssenceEventHandler.kt
@@ -12,13 +12,12 @@ import net.minecraftforge.event.world.ChunkWatchEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
-@Suppress("unused")
+@Suppress("unused", "SpellCheckingInspection")
 @Mod.EventBusSubscriber(modid = Reference.MOD_ID)
 object ChunkBugEssenceEventHandler {
 	@SubscribeEvent
 	fun onChunkWatchEvent(event: ChunkWatchEvent) {
 		val chunk = event.chunkInstance ?: return
-
 		val bugEssence = BugEssenceCapability.isCapable(chunk)
 
 		if (bugEssence != null) {
@@ -26,51 +25,40 @@ object ChunkBugEssenceEventHandler {
 			if (bugEssence.max > 0 || bugEssence.current > 0) return
 
 			var max = 0
-			var current = 0
 
-			@Suppress("SpellCheckingInspection")
 			for (biomeID in chunk.biomeArray.toSet()) {
 				when (Biome.REGISTRY.getObjectById(biomeID.toInt())) {
 					is BiomeMushroomIsland -> {
 						max += 3000
-						current += 2000
 					}
 					is BiomeSwamp -> {
 						max += 1600
-						current += 1000
 					}
 					is BiomePlains -> {
 						max += 1000
-						current += 200
 					}
 					is BiomeForest -> {
 						max += 800
-						current += 140
 					}
 					is BiomeJungle -> {
 						max += 600
-						current += 140
 					}
 					is BiomeSavanna, is BiomeTaiga -> {
 						max += 400
-						current += 60
 					}
 					is BiomeDesert, is BiomeMesa,
 					is BiomeHills -> {
 						max += 200
-						current += 24
 					}
 					is BiomeRiver, is BiomeBeach,
 					is BiomeOcean, is BiomeSnow,
 					is BiomeStoneBeach -> {
 						max += 100
-						current += 8
 					}
 				}
 			}
 
 			bugEssence.max = max
-			bugEssence.current = current
 
 			for (player in chunk.world.playerEntities) {
 				BugMagic.CHANNEL.sendTo(
